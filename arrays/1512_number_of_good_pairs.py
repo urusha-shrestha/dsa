243572a4c22189f523253c2ef424b72bbd5f34af
Urusha <urusha10@gmail.com>
2024-08-27 10:31:49 -0400

    number of good pairs

---
+++ arrays/1512_number_of_good_pairs.py
@@ -0,0 +1,18 @@
+def goodPairs(nums):
+    l=0
+    r=len(nums)-1
+    counter=0
+    while l<len(nums)-1:
+        r=len(nums)-1
+        while l<r:
+            if nums[l]==nums[r]:
+                print(l,r)
+                counter += 1
+            r -= 1
+        l += 1
+    return counter
+    
+        
+
+nums = [1,1,1,1]
+print(goodPairs(nums))
