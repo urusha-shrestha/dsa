1a27f9cc34d6bd69656bddeba8701f5a7d4ac1f2
Urusha <urusha10@gmail.com>
2025-01-25 12:59:44 -0500

    implement queue from stack

---
+++ queue/232_implement_queue_using_stacks.py
@@ -0,0 +1,36 @@
+# Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+# Implement the MyQueue class:
+
+# void push(int x) Pushes element x to the back of the queue.
+# int pop() Removes the element from the front of the queue and returns it.
+# int peek() Returns the element at the front of the queue.
+# boolean empty() Returns true if the queue is empty, false otherwise.
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self):
+        return self.s1.pop()
+    
+    def peek(self):
+        return self.s1[-1]
+    
+    def empty(self):
+        return not self.s1
+
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+obj.push(3)
+obj.push(4)
